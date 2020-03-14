67a2d05ae94edc4ff83a0b604a5ba355b054477c
Alejandro III de Macedonia <alejandro@Alejandros-MacBook-Pro.local>
2020-03-14 15:27:10 -0600

    changed shoot mechanic

--- player/player.gd
+++ player/player.gd
@@ -8,8 +8,8 @@ var TYPE = 'player'
 var WEAPON_RADIUS = 113
 var motion_dir = Vector2(0, 0)
 onready var projectile_class = preload('res://weapons/projectile.tscn')
-var charge_timer = Timer.new()
 var CHARGE_WAIT_TIME = 1.0
+var MAX_SHOOT_LENGTH = 200
 onready var items = {
 	globals.ITEM_TYPES.SUPPORT: 0,
 	globals.ITEM_TYPES.BOOST: 0,
@@ -21,13 +21,13 @@ enum STATE {
 	charging
 }
 var current_state = STATE.idle
+var shoot_point_start = null
 
 var health = 10.0
 
 
 func _ready():
-	charge_timer.connect('timeout', self, '_on_charge_timer_timeout')
-	add_child(charge_timer)
+	pass
 
 func addItem(item):
 	items[item.TYPE] += 1
@@ -42,9 +42,8 @@ func movementLoop():
 	move_and_slide(motion)
 
 
-func attack(power):
+func attack(power, direction):
 	var projectile = projectile_class.instance()
-	var direction = (get_global_mouse_position() - position).normalized()
 	var offset = direction * WEAPON_RADIUS
 
 	projectile.position = position + offset
@@ -66,24 +65,37 @@ func pollInput():
 	motion_dir =  Vector2(X, Y)
 
 	if Input.is_action_pressed('touch') and current_state != STATE.charging:
-		charge_timer.set_wait_time(CHARGE_WAIT_TIME)
-		charge_timer.start()
 		current_state = STATE.charging
+		shoot_point_start = get_global_mouse_position()
+		
 
 	if Input.is_action_just_released('touch') and current_state == STATE.charging:
-		var partial_power = (CHARGE_WAIT_TIME - charge_timer.get_time_left())/CHARGE_WAIT_TIME
+		var shoot_length = (get_global_mouse_position() - shoot_point_start).length()
+		shoot_length = shoot_length if shoot_length <= MAX_SHOOT_LENGTH else MAX_SHOOT_LENGTH
+		var partial_power = shoot_length/MAX_SHOOT_LENGTH
 		var power = 0.25 + 0.75 * partial_power
 
 		current_state = STATE.idle
-		charge_timer.stop()
 		if power >= 0.5:
-			attack(power)
+			var direction = (shoot_point_start - get_global_mouse_position()).normalized()
+			attack(power, direction)
 
 
-func _on_charge_timer_timeout():
-	charge_timer.stop()
-	print('READY')
+func _draw():
+	if current_state != STATE.charging:
+		return
+	draw_circle(
+		shoot_point_start - position,
+		(get_global_mouse_position() - shoot_point_start).length(),
+		Color(1, 1, 1, 0.5)
+	)
+	var color = Color(1, 1, 1) if (get_global_mouse_position() - shoot_point_start).length() < MAX_SHOOT_LENGTH else Color(1, 0, 0)
+	draw_line(Vector2(0, 0), shoot_point_start - get_global_mouse_position(), color)
 
+
+
+func _process(delta):
+	update()
 
 func _physics_process(delta):
 	pollInput()
