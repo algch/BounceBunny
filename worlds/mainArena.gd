fa8b7618d5efca2288764266a7f2e2009295f8a1
Alejandro III de Macedonia <alejandro@Alejandros-MacBook-Pro.local>
2020-04-10 19:06:47 -0500

    debug

--- worlds/mainArena.gd
+++ worlds/mainArena.gd
@@ -22,11 +22,11 @@ func _ready():
 	get_tree().connect('network_peer_disconnected', self, '_on_player_disconnected')
 	get_tree().connect('network_peer_connected', self, '_on_player_connected')
 	if is_server:
-		create_server()
+		createServer()
 	else:
 		connectToServer()
 
-func create_server():
+func createServer():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_server(DEFAULT_PORT, MAX_PLAYERS)
 	get_tree().set_network_peer(peer)
@@ -46,41 +46,44 @@ func connectToServer(): # 1
 	peer.create_client(ip_address, DEFAULT_PORT)
 	get_tree().set_network_peer(peer)
 
-func _connected_to_server(): # when does this happen?
-	print('_connected_to_server called')
+func _connected_to_server(): # this happens async
+	print('we have connected to server')
 	var local_player_id = get_tree().get_network_unique_id()
-	print('created a new graph ', local_player_id)
-	print('all graphs ', all_graphs)
-	var pos = Vector2(10 + randi()%500, 10 + randi()%500)
-	print('position of new graph ', pos)
-	rpc('_send_player_info', local_player_id, pos)
+	var pos = Vector2(50 + randi()%500, 50 + randi()%500)
+	rpc_id(1, 'printInServer', 'a message from the client')
 
-remote func _send_player_info(id, pos): # 3
-	attachNewGraph(id)
-	print('remote _send_player_info')
-	print(id, all_graphs, pos)
-	var new_plant = load('res://plants/plant.tscn').instance()
-	add_child(new_plant)
-	new_plant.init(pos)
-	var new_player = load('res://player/player.tscn').instance()
-	new_player.name = str(id)
-	new_player.set_network_master(id)
-	add_child(new_player)
-	new_player.init('jugador', pos, new_plant)
+# remote func _send_player_info(id, pos): # 3
+# 	attachNewGraph(id)
+# 	print('remote _send_player_info')
+# 	print(id, all_graphs, pos)
+# 	var new_plant = load('res://plants/plant.tscn').instance()
+# 	add_child(new_plant)
+# 	new_plant.init(pos)
+# 	var new_player = load('res://player/player.tscn').instance()
+# 	new_player.name = str(id)
+# 	new_player.set_network_master(id)
+# 	add_child(new_player)
+# 	new_player.init('jugador', pos, new_plant)
 
-func _on_player_connected(connected_player_id): # 2
-	print('_on_player_connected ', connected_player_id)
-	var local_player_id = get_tree().get_network_unique_id()
-	if not get_tree().is_network_server():
-		print('I am a client!')
-		rpc_id(1, '_request_player_info', local_player_id, connected_player_id)
+func _on_player_connected(connected_player_id):
+	print('a player has conected', connected_player_id)
+	# var local_player_id = get_tree().get_network_unique_id()
+	# if not get_tree().is_network_server():
+	# 	print('I am a client!')
+	# 	rpc_id(1, 'printInServer', 'a message from the client')
+		# rpc_id(1, '_request_player_info', local_player_id, connected_player_id)
 
-remote func _request_player_info(request_from_id, player_id):
-	print('_request_player_info ', request_from_id, player_id)
-	if get_tree().is_network_server():
-		print('I am a server!')
-		var pos = Vector2(10 + randi()%500, 10 + randi()%500)
-		rpc_id(request_from_id, '_send_player_info', player_id, pos)
+remote func printInServer(message):
+	print(message)
+
+# remote func _request_player_info(request_from_id, player_id):
+# 	print('_request_player_info ', request_from_id, player_id)
+# 	if get_tree().is_network_server():
+# 		print('I am a server!')
+# 		var pos = Vector2(10 + randi()%500, 10 + randi()%500)
+# 		print('calling _send_player_info to ', request_from_id)
+# 		rpc_id(request_from_id, '_send_player_info', player_id, pos)
 
 func _on_player_disconnected(id):
-	all_graphs.erase(id)
+	print('player ', id, ' disconnected')
+	# all_graphs.erase(id)
